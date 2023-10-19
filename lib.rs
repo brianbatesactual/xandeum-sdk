07a8d5284c81701fec29ee9d27efc6ebfbee223c
Brian <brian_bates@me.com>
2023-10-19 15:35:19 -0700

    init

--- lib.rs
+++ lib.rs
@@ -1,50 +1,50 @@
-//! The Solana host and client SDK.
+//! The Xandeum host and client SDK.
 //!
 //! This is the base library for all off-chain programs that interact with
-//! Solana or otherwise operate on Solana data structures. On-chain programs
-//! instead use the [`solana-program`] crate, the modules of which are
+//! Xandeum or otherwise operate on Xandeum data structures. On-chain programs
+//! instead use the [`xandeum-program`] crate, the modules of which are
 //! re-exported by this crate, like the relationship between the Rust
 //! `core` and `std` crates. As much of the functionality of this crate is
-//! provided by `solana-program`, see that crate's documentation for an
+//! provided by `xandeum-program`, see that crate's documentation for an
 //! overview.
 //!
-//! [`solana-program`]: https://docs.rs/solana-program
+//! [`xandeum-program`]: https://docs.rs/xandeum-program (TBD)
 //!
-//! Many of the modules in this crate are primarily of use to the Solana runtime
-//! itself. Additional crates provide capabilities built on `solana-sdk`, and
+//! Many of the modules in this crate are primarily of use to the xandeum runtime
+//! itself. Additional crates provide capabilities built on `xandeum-sdk`, and
 //! many programs will need to link to those crates as well, particularly for
-//! clients communicating with Solana nodes over RPC.
+//! clients communicating with Xandeum nodes over RPC.
 //!
 //! Such crates include:
 //!
-//! - [`solana-client`] - For interacting with a Solana node via the [JSON-RPC API][json].
-//! - [`solana-cli-config`] - Loading and saving the Solana CLI configuration file.
-//! - [`solana-clap-utils`] - Routines for setting up the CLI using [`clap`], as
-//!   used by the Solana CLI. Includes functions for loading all types of
+//! - [`xandeum-client`] - For interacting with a Xandeum node via the [JSON-RPC API][json].
+//! - [`xandeum-cli-config`] - Loading and saving the Xandeum CLI configuration file.
+//! - [`xandeum-clap-utils`] - Routines for setting up the CLI using [`clap`], as
+//!   used by the xandeum CLI. Includes functions for loading all types of
 //!   signers supported by the CLI.
 //!
-//! [`solana-client`]: https://docs.rs/solana-client
-//! [`solana-cli-config`]: https://docs.rs/solana-cli-config
-//! [`solana-clap-utils`]: https://docs.rs/solana-clap-utils
-//! [json]: https://docs.solana.com/developing/clients/jsonrpc-api
+//! [`xandeum-client`]: https://docs.rs/xandeum-client
+//! [`xandeum-cli-config`]: https://docs.rs/xandeum-cli-config
+//! [`xandeum-clap-utils`]: https://docs.rs/xandeum-clap-utils
+//! [json]: https://docs.xandeum.com/developing/clients/jsonrpc-api
 //! [`clap`]: https://docs.rs/clap
 
 #![allow(incomplete_features)]
 #![cfg_attr(RUSTC_WITH_SPECIALIZATION, feature(specialization))]
 #![cfg_attr(RUSTC_NEEDS_PROC_MACRO_HYGIENE, feature(proc_macro_hygiene))]
 
-// Allows macro expansion of `use ::solana_sdk::*` to work within this crate
-extern crate self as solana_sdk;
+// Allows macro expansion of `use ::xandeum_sdk::*` to work within this crate
+extern crate self as xandeum_sdk;
 
 #[cfg(feature = "full")]
 pub use signer::signers;
-// These solana_program imports could be *-imported, but that causes a bunch of
+// These xandeum_program imports could be *-imported, but that causes a bunch of
 // confusing duplication in the docs due to a rustdoc bug. #26211
 #[allow(deprecated)]
-pub use solana_program::address_lookup_table_account;
-#[cfg(not(target_os = "solana"))]
-pub use solana_program::program_stubs;
-pub use solana_program::{
+pub use xandeum_program::address_lookup_table_account;
+#[cfg(not(target_os = "xandeum"))]
+pub use xandeum_program::program_stubs;
+pub use xandeum_program::{
     account_info, address_lookup_table, alt_bn128, big_mod_exp, blake3, borsh, borsh0_10, borsh0_9,
     bpf_loader, bpf_loader_deprecated, bpf_loader_upgradeable, clock, config, custom_heap_default,
     custom_panic_default, debug_account_data, declare_deprecated_sysvar_id, declare_sysvar_id,
@@ -103,7 +103,7 @@ pub mod transport;
 pub mod wasm;
 
 /// Same as `declare_id` except report that this id has been deprecated.
-pub use solana_sdk_macro::declare_deprecated_id;
+pub use xandeum_sdk_macro::declare_deprecated_id;
 /// Convenience macro to declare a static public key and functions to interact with it.
 ///
 /// Input: a single literal base58 string representation of a program's id
@@ -114,10 +114,10 @@ pub use solana_sdk_macro::declare_deprecated_id;
 /// # // wrapper is used so that the macro invocation occurs in the item position
 /// # // rather than in the statement position which isn't allowed.
 /// use std::str::FromStr;
-/// use solana_sdk::{declare_id, pubkey::Pubkey};
+/// use xandeum_sdk::{declare_id, pubkey::Pubkey};
 ///
 /// # mod item_wrapper {
-/// #   use solana_sdk::declare_id;
+/// #   use xandeum_sdk::declare_id;
 /// declare_id!("My11111111111111111111111111111111111111111");
 /// # }
 /// # use item_wrapper::id;
@@ -125,7 +125,7 @@ pub use solana_sdk_macro::declare_deprecated_id;
 /// let my_id = Pubkey::from_str("My11111111111111111111111111111111111111111").unwrap();
 /// assert_eq!(id(), my_id);
 /// ```
-pub use solana_sdk_macro::declare_id;
+pub use xandeum_sdk_macro::declare_id;
 /// Convenience macro to define a static public key.
 ///
 /// Input: a single literal base58 string representation of a Pubkey
@@ -134,20 +134,20 @@ pub use solana_sdk_macro::declare_id;
 ///
 /// ```
 /// use std::str::FromStr;
-/// use solana_program::{pubkey, pubkey::Pubkey};
+/// use xandeum_program::{pubkey, pubkey::Pubkey};
 ///
 /// static ID: Pubkey = pubkey!("My11111111111111111111111111111111111111111");
 ///
 /// let my_id = Pubkey::from_str("My11111111111111111111111111111111111111111").unwrap();
 /// assert_eq!(ID, my_id);
 /// ```
-pub use solana_sdk_macro::pubkey;
+pub use xandeum_sdk_macro::pubkey;
 /// Convenience macro to define multiple static public keys.
-pub use solana_sdk_macro::pubkeys;
+pub use xandeum_sdk_macro::pubkeys;
 #[rustversion::since(1.46.0)]
-pub use solana_sdk_macro::respan;
+pub use xandeum_sdk_macro::respan;
 
-// Unused `solana_sdk::program_stubs!()` macro retained for source backwards compatibility with older programs
+// Unused `xandeum_sdk::program_stubs!()` macro retained for source backwards compatibility with older programs
 #[macro_export]
 #[deprecated(
     since = "1.4.3",
@@ -172,7 +172,7 @@ pub extern crate bs58;
 extern crate log as logger;
 
 #[macro_use]
-extern crate solana_frozen_abi_macro;
+extern crate xandeum_frozen_abi_macro;
 
 #[cfg(test)]
 mod tests {
